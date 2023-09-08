65e5bfbec61e43504bce639fe4fbeb9c7aa80573
Dontbeshy27 <eduardopachecojr913@gmail.com>
2023-09-07 18:23:30 -0800

    api

--- OUR_CAPSTONE/routes/api.php
+++ OUR_CAPSTONE/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthenticationApiController;
+use App\Http\Controllers\EmployeeApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+// }); 
+    Route::post('/logout', [AuthenticationApiController::class, 'logout']); 
+    Route::get('/employees', [EmployeeApiController::class, 'index']);
+    Route::post('/employees', [EmployeeApiController::class, 'store']);
+    Route::get('/employees/{employee}', [EmployeeApiController::class, 'show']);
+    Route::put('/employees/{employee}', [EmployeeApiController::class, 'update']);
+    Route::patch('/employees/{employee}', [EmployeeApiController::class, 'update']);
+    Route::delete('/employees/{employee}', [EmployeeApiController::class, 'destroy']); 
+}); //LOck the DATA 
+Route::post('/login', [AuthenticationApiController::class, 'login']);
