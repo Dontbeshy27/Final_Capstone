4e06ab80ad027e82b543bf4180b05641187f87cb
Dontbeshy27 <eduardopachecojr913@gmail.com>
2023-09-10 00:22:49 -0800

    databaseseeder

--- OUR_CAPSTONE/database/seeders/DatabaseSeeder.php
+++ OUR_CAPSTONE/database/seeders/DatabaseSeeder.php
@@ -25,7 +25,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(adminsSeeder::class);
-        $this->call(Pubg_ClansSeeder::class);
     }
 }
